34cc65ef1714f0cdb8430e474f507973f3dc0cf1
Austin-Cathey <austincatheyart@gmail.com>
2023-12-27 15:29:27 -0500

    strikeout working! Style needs work though

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,14 +1,36 @@
 <script setup>
 import { ref } from 'vue'
 
+/* const purchased = ref(false) */
 const props = defineProps({ list: Object })
+
+const togglePurchased = (items) => {
+  items.purchased = !items.purchased
+}
 </script>
 
 <template>
   <h3>{{ props.list.title }}</h3>
   <ul>
-    <li v-for="items in props.list.items" key="list.items.id">
+    <li
+      v-for="items in props.list.items"
+      @click="togglePurchased(items)"
+      :key="list.items.id"
+      :class="{ strikeout: items.purchased }"
+    >
       {{ items.itemName }}
+      <!--       <input v-model="purchased" type="checkbox" /> -->
     </li>
   </ul>
 </template>
+
+<style>
+.strikeout {
+  text-decoration: line-through;
+  color: #b8c2cc;
+}
+
+.strikeout:hover {
+  color: #8795a1;
+}
+</style>
